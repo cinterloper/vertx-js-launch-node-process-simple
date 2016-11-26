ef3ac6daf2a537f0b1ee168db83c5b2dabd72884
Grant Haywood <grant@iowntheinter.net>
2016-11-26 15:23:23 -0700

    ..

--- README.sh
+++ README.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 npm install vertx3-eventbus-client .
 _JAVA_OPTIONS='-Dnashorn.args=-scripting' vertx run --worker vertx-launcher.js
 #Picked up _JAVA_OPTIONS: -Dnashorn.args=-scripting
